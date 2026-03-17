15db2a7eae2c5666ae736304acf9707c75b33d30
Poojithamenon06 <poojithakmenon2006@gmail.com>
2026-03-17 09:45:29 +0530

    CORS allow all - Edited

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,14 @@ connectDB();
 // };
 
 // app.use(cors(corsOptions));
-app.use(cors());
+
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+app.options('*', cors());
+
 app.use(express.json());
 app.use('/api', rateLimit({ windowMs: 15 * 60 * 1000, max: 200 }));
 
